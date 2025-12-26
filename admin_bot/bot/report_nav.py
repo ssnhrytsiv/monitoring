f5f246e8918dd66d69c91d806d2ad95834907ca5
ssnhrytsiv <nazar.hrytsiv7@gmail.com>
2025-12-26 23:52:44 +0300

    fixed duplicate4

--- admin_bot/bot/report_nav.py
+++ admin_bot/bot/report_nav.py
@@ -73,6 +73,7 @@ async def cb_report_show_sections(cb: CallbackQuery):
         pages[report_idx],
         disable_web_page_preview=True,
         reply_markup=kb,
+        parse_mode="HTML",
     )
     await _safe_answer(cb)
 
@@ -102,5 +103,6 @@ async def cb_report_page_nav(cb: CallbackQuery):
         pages[cur],
         disable_web_page_preview=True,
         reply_markup=kb,
+        parse_mode="HTML",
     )
     await _safe_answer(cb)
